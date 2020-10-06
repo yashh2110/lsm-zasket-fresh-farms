e24bee89d944b5362962ae6f606ee651886aaad1
manideva6@gmail.com <>
2020-10-06 15:31:44 +0530

    app renamed to zasket

--- android/app/src/main/java/com/zasket/MainActivity.java
+++ android/app/src/main/java/com/zasket/MainActivity.java
@@ -1,4 +1,4 @@
-package com.greentomato;
+package com.zasket;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "GreenTomato";
+    return "Zasket";
   }
 }
--- android/app/src/main/java/com/zasket/MainApplication.java
+++ android/app/src/main/java/com/zasket/MainApplication.java
@@ -1,4 +1,4 @@
-package com.greentomato;
+package com.zasket;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.greentomato.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.zasket.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
