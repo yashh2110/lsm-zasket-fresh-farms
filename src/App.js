6ddf58f76ad1c2b5a02a0eccaca5f59619d6d711
RAGUL R S <ragulrs@RAGULs-MBP.bbrouter>
2021-07-29 10:33:37 -0700

    cart

--- src/App.js
+++ src/App.js
@@ -15,8 +15,13 @@ import RNAndroidLocationEnabler from 'react-native-android-location-enabler';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import * as Sentry from "@sentry/react-native";
 import RNUxcam from 'react-native-ux-cam';
+RNUxcam.enableAdvancedGestureRecognizers = (enable) => void
+// Example
+// Set to FALSE before startWithKey to disable - Default is TRUE
+RNUxcam.enableAdvancedGestureRecognizers(false);
 RNUxcam.optIntoSchematicRecordings(); // Add this line to enable iOS screen recordings
 RNUxcam.startWithKey('qercwheqrlqze96'); // Add this line after RNUxcam.optIntoSchematicRecordings();
+
 const App = () => {
 
   useEffect(() => {
