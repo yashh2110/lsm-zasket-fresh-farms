777f1fb89cb995ff1a635ff9d09048fd6dc695e2
RAGUL R S <ragulrs@RAGULs-MBP.bbrouter>
2022-01-15 22:47:10 +0530

    review for playstore and appstore is complted

--- src/components/AccountStack/RateOrdersScreen.js
+++ src/components/AccountStack/RateOrdersScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { TouchableOpacity, StyleSheet, View, Text, FlatList, Dimensions, Image, TextInput } from 'react-native';
+import { TouchableOpacity, StyleSheet, View, Text, FlatList, Dimensions, Image, TextInput, Linking, Platform } from 'react-native';
 import { Icon, Toast } from 'native-base'
 import Modal from 'react-native-modal';
 import Theme from "../../styles/Theme";
@@ -12,6 +12,7 @@ import Loader from "../common/Loader";
 import moment from 'moment'
 import StarRating from 'react-native-star-rating';
 import { rateOrder } from '../../actions/cart'
+import InAppReview from 'react-native-in-app-review';
 const RateOrdersScreen = ({ route, navigation, config, rateOrder }) => {
     const { item } = route?.params;
     const [rating, setRating] = useState(0)
@@ -25,12 +26,63 @@ const RateOrdersScreen = ({ route, navigation, config, rateOrder }) => {
         rateOrder(item?.id, rateOrderRequest, (res, status) => {
             if (status) {
                 // alert(JSON.stringify(res))
+                if (rating == 5) {
+                    InAppReview.isAvailable();
+                    // trigger UI InAppreview
+                    InAppReview.RequestInAppReview()
+                        .then((hasFlowFinishedSuccessfully) => {
+                            // when return true in android it means user finished or close review flow
+                            console.log('InAppReview in android', hasFlowFinishedSuccessfully);
+                            // when return true in ios it means review flow lanuched to user.
+                            console.log(
+                                'InAppReview in ios has lanuched successfully',
+                                hasFlowFinishedSuccessfully,
+                            );
+                            // 1- you have option to do something ex: (navigate Home page) (in android).
+                            // 2- you have option to do something,
+                            // ex: (save date today to lanuch InAppReview after 15 days) (in android and ios).
+
+                            // 3- another option:
+                            if (hasFlowFinishedSuccessfully) {
+                                // do something for ios
+                                // do something for android
+                            }
+                            // for android:
+                            // The flow has finished. The API does not indicate whether the user
+                            // reviewed or not, or even whether the review dialog was shown. Thus, no
+                            // matter the result, we continue our app flow.
+
+                            // for ios
+                            // the flow lanuched successfully, The API does not indicate whether the user
+                            // reviewed or not, or he/she closed flow yet as android, Thus, no
+                            // matter the result, we continue our app flow.
+                        })
+                        .catch((error) => {
+                            //we continue our app flow.
+                            // we have some error could happen while lanuching InAppReview,
+                            // Check table for errors and code number that can return in catch.
+                            console.log("lanuchingInAppReview", error);
+                            updateAppNotice()
+                        });
+
+                }
+
                 navigation.goBack()
             } else {
                 // alert(JSON.stringify(res?.response))
                 // console.warn(JSON.stringify(res?.response, null, "      "))
             }
         })
+    }
+    const updateAppNotice = async () => {
+        const APP_STORE_LINK = 'https://apps.apple.com/in/app/zasket/id1541056118';
+        const PLAY_STORE_LINK = 'market://details?id=com.zasket';
+        if (Platform.OS == 'android') {
+            Linking.openURL(PLAY_STORE_LINK)
+        } else {
+            Linking.openURL(APP_STORE_LINK)
+
+        }
     }
 
     return (
