23f67db7b9fca528f6319135218f7dade2d77245
Yashwanth Muddana <muddana.yashwanth@gmail.com>
2022-02-04 19:49:34 +0530

    version_1.3.3

--- env.ts
+++ env.ts
@@ -5,7 +5,7 @@ export const baseURL = "https://test-api.zasket.in";
 export const MapApiKey = "AIzaSyCCv96lsG-ywwvtHz9M0JmU4Uyzxl17Vuc";
 export const RazorpayTestApiKey = "rzp_test_C64YcthVL1POFJ"; //not used inside code
 export const RazorpayLiveApiKey = "rzp_live_U8JU5T86HWykhV"; //not used inside code
-export const appVersion = "1.3.2"; //update the version of app at each time u upload to appstore or playstore
+export const appVersion = "1.3.3"; //update the version of app at each time u upload to appstore or playstore
 export const OneSignalAppId = "339a3ed7-ed7c-4f98-84bf-9fa326a1c6d8";
 //LocalStorage Mentions
 // userDetails
